743886b87c5e4edfbafc56b729dc832b0c438374
Phillippe Rocha <phillipperocha@gmail.com>
2020-05-09 13:13:20 -0300

    Date validation

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,18 +1,40 @@
 import { Router } from 'express';
+// Vamos importar duas funcionalidades do date-fns
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+// Precisamos utilizar tipagem no appointments através de uma interface
+interface Appointment {
+  id: number;
+  provider: string;
+  date: Date;
+}
+
+// E declaramos agora que a variável o tipo dela é um array de Appointments
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
-  // Vamos fazer apenas uma rota de teste
-  // provider é o prestador de serviço, date
   const { provider, date } = request.body;
+
+  // Deixaremos a data no formato Iso e queremos no começo da hora dela
+  const parsedDate = startOfHour(parseISO(date));
+
+  // Verificaremos se há horários já marcados
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date)
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is alredy booked.' });
+  }
 
   const appointment = {
     id: Math.random() * 10000,
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
