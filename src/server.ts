6d5e16026bf923cf499c406aee7fc7853fdb6700
Phillippe Rocha <phillipperocha@gmail.com>
2020-05-09 00:59:49 -0300

    Debugger setup

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import routes from './routes';
 
 const app = express();
 
-// O routes vira como se fosse um middleware do projeto
-// E vamos adicionar todas as rotas definidas no arquivo routes
-// dentro do nosso app.
+// Agora a aplicação vai aceitar JSON
+app.use(express.json());
+
 app.use(routes);
 
 app.listen(3333, () => {
