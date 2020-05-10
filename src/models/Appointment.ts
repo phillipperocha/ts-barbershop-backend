2363d0df9865bf3d1bf45171208a9ae60a431835
Phillippe Rocha <phillipperocha@gmail.com>
2020-05-10 12:13:32 -0300

    Creating appointment model with TS decorators

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,19 +1,15 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   provider: string;
 
+  @Column('timestamp with time zone')
   date: Date;
-
-  // O typescript tem uma função helper chamada Omit que
-  // receberá os generics informando a classe para se basear
-  // em seguida quais os parâmetros que queremos omitir.
-
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = (Math.random() * 1000000).toFixed();
-    this.provider = provider;
-    this.date = date;
-  }
 }
 
 export default Appointment;
