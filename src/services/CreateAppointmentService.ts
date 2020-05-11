71bf0b775df4b8433675f4dc46b702289244dd3e
Phillippe Rocha <phillipperocha@gmail.com>
2020-05-10 23:32:28 -0300

    Encrypting user password

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -10,8 +10,6 @@ interface Request {
 }
 
 class CreateAppointmentService {
-  // Como utilizaremos await, o método execute() precisa ser assíncrono
-  // E retornar uma promise
   public async execute({ date, provider_id }: Request): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
@@ -25,13 +23,11 @@ class CreateAppointmentService {
       throw Error('This appointment is alredy booked');
     }
 
-    // O método create() não salva por padrão só cria em memória
     const appointment = appointmentsRepository.create({
       provider_id,
       date,
     });
 
-    // Precisamos utilizar o método .save() do appointmentsRepository
     await appointmentsRepository.save(appointment);
 
     return appointment;
