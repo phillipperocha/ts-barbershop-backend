5eb710a1fc14bd7e6bbc443dab5b0bd2dfe89514
Phillippe Rocha <phillipperocha@gmail.com>
2020-05-12 09:49:56 -0300

    Authenticate routes

---
+++ src/config/auth.ts
@@ -0,0 +1,10 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  jwt: {
+    secret: `${process.env.APP_SECRET}`,
+    expiresIn: '7d',
+  },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -30,9 +31,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect password.');
     }
 
-    const token = sign({}, process.env.APP_SECRET, {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '7d',
+      expiresIn,
     });
 
     return {
