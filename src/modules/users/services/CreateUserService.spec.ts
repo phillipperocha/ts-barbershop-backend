3f7937cb1bb604f23f57fdb29173456ee83092a7
Phillippe Rocha <phillipperocha@gmail.com>
2020-05-18 21:06:26 -0300

    Testing auth

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,13 +1,18 @@
 import AppError from '@shared/errors/AppError';
 
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import CreateUserService from './CreateUserService';
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
 
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
 
     const user = await createUser.execute({
       name: 'John Doe',
@@ -20,8 +25,12 @@ describe('CreateUser', () => {
 
   it('should not be able to create a new user with used email', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
 
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
 
     await createUser.execute({
       name: 'John Doe',
