0e30978c5c53c5cf4defe7e8113664c1591e9f3a
Phillippe Rocha <phillipperocha@gmail.com>
2020-05-27 21:34:57 -0300

    Provider appointments

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,12 +2,18 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
+// Vamos importar o nosso novo controller
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+// instanciá-lo
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/', appointmentsController.create);
+// E vamos criar a nossa rota pra pegar do próprio provider
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
